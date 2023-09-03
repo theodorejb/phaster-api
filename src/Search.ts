f37cdb2ea90d8bc92a55dcc8cfdfac8bfd2e99cf
Theodore Brown <theodorejb@outlook.com>
2023-09-03 00:35:46 -0500

    Add search helper functions

---
+++ src/Search.ts
@@ -0,0 +1,61 @@
+import { SearchResponse } from "./Endpoint";
+
+export function getApiParams(params: URLSearchParams, limitKey = "pageSize") {
+    const limit = +(params.get("limit") ?? localStorage.getItem(limitKey) ?? 25);
+    const page = +(params.get("page") ?? 1);
+    const offset = limit * page - limit;
+    const apiParams = new URLSearchParams();
+    apiParams.set("limit", limit.toString());
+    apiParams.set("offset", offset.toString());
+
+    // add any sort params
+    params.forEach((value, key) => {
+        if (key.startsWith("sort[")) {
+            apiParams.set(key, value);
+        }
+    });
+
+    return {
+        apiParams,
+        limit,
+        page,
+    };
+}
+
+export interface SearchPageData<T> {
+    items: T[];
+    params: { [key: string]: string };
+    pages: number;
+    page: number;
+    limit: number;
+    error: string;
+}
+
+export async function getSearchData<T>(
+    response: Promise<SearchResponse<T>>,
+    params: URLSearchParams,
+    limit: number,
+    page: number,
+): Promise<SearchPageData<T>> {
+    let items: T[] = [];
+    let lastPage = true;
+    let error = "";
+
+    try {
+        const resp = await response;
+        items = resp.data;
+        lastPage = resp.lastPage;
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (e: any) {
+        error = e.message;
+    }
+
+    return {
+        items,
+        params: Object.fromEntries(params),
+        pages: lastPage ? page : page + 1,
+        page,
+        limit,
+        error,
+    };
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from "./Api.js";
 export * from "./Endpoint.js";
+export * from "./Search.js";
